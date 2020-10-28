5bb9ef2395da3ac0692530f59a7fb87fe66f7807
Veda Bagade <veda.bagade001@gmail.com>
2020-10-28 14:07:23 +0530

    constraint added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,17 +35,27 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    constrainedLog = new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
 
+    var options = {
+        bodyA: bird.body,
+        bodyB: constrainedLog.body,
+        stiffness: 0.04,
+        length: 10
+    }
+
+    var chain = Constraint.create(options);
+    World.add(world,chain);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -58,7 +70,11 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    constrainedLog.display();
 
     bird.display();
     platform.display();
+
+    strokeWeight(3);
+    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
